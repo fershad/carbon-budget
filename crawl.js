87d0f0446d6f55e673275258729750a2a2db8112
Fershad <itsfish@fershad.com>
2022-10-15 19:16:50 +0800

    remove unused const

--- crawl.js
+++ crawl.js
@@ -1,8 +1,6 @@
 import Crawler from 'simplecrawler';
 
 
-const urls = [];
-
 export const isContentTypeHtml = (contentType) => {
     return contentType?.toLowerCase().includes('html');
   };
@@ -16,8 +14,6 @@ export const crawl = (siteUrl) => {
     if (!isContentTypeHtml(contentType)) return;
     const statusCode = response.statusCode;
     if (!contentType || !statusCode) return;
-    console.log('urlFound', url, contentType, responseBuffer.length, statusCode)
-    urls.push(url);
   });
 
   crawler.on('complete', () => urls);
