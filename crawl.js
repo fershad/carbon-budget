853b38b93a1afec051f1590a4211ff27ffa55e9a
Fershad <itsfish@fershad.com>
2022-10-15 21:03:01 +0800

    working scan + lh test with result output

--- crawl.js
+++ crawl.js
@@ -1,5 +1,8 @@
 import Crawler from 'simplecrawler';
+import { runLighthouse, writeLHResult } from './lh.js';
 
+const lighthouse = runLighthouse
+const urls = []
 
 export const isContentTypeHtml = (contentType) => {
     return contentType?.toLowerCase().includes('html');
@@ -14,9 +17,19 @@ export const crawl = (siteUrl) => {
     if (!isContentTypeHtml(contentType)) return;
     const statusCode = response.statusCode;
     if (!contentType || !statusCode) return;
+
+    urls.push(
+      url
+    );
   });
 
-  crawler.on('complete', () => urls);
+  crawler.on('complete', async () => {
+    console.log(urls);
+    for (const url of urls) {
+      const result = await lighthouse(url);
+      await writeLHResult(result, url);
+    }
+  });
 
   crawler.start();
 };
---
+++ lh.js
@@ -0,0 +1,140 @@
+import fs from 'fs'
+import shortHash from 'short-hash'
+import lighthouse from "lighthouse";
+import chromeLauncher from "chrome-launcher";
+import desktopConfig from "./node_modules/lighthouse/lighthouse-core/config/lr-desktop-config.js";
+
+const SECONDS = 1000;
+
+/**
+   * @param  {URL} url - a url as parsed using the node WHATWG URL API
+   *
+   * Accept an url, and use chrome to run a Lighthouse against the given url
+   * using a slightly modified desktop config. With this config, we account
+   * for very slow pages, to make it possible to check a url against the
+   * Web Archive.
+   */
+ export async function runLighthouse(url) {
+  const chrome = await chromeLauncher.launch({ chromeFlags: ["--headless"] });
+
+  // override desktop to allow for looooong load times
+  // from the webarchive
+  desktopConfig.settings.maxWaitForFcp = 180 * SECONDS;
+  desktopConfig.settings.maxWaitForLoad = 180 * SECONDS;
+
+  const options = { logLevel: "none", output: "html", port: chrome.port };
+
+  const runnerResult = await lighthouse(url, options, desktopConfig);
+
+  await chrome.kill();
+  return runnerResult;
+}
+
+/**
+ * @param  {LighthouseResultObject} runnerResult - a Lighthouse Result object
+ * @param  {string} reportName -a string for the path on the file system to use.
+ *
+ * Take the Lighthouse result and write the human readable HTML report, as well as
+ * dumping the JSON representation.
+ * We assume `reportName` has been sanitised and is safe to use.
+ */
+ export async function writeLHResult(runnerResult, reportName) {
+  const reportHTML = runnerResult.report;
+  const date = new Date().toISOString().replace(/:/g, "-");
+  const reportJSON = JSON.stringify(runnerResult.lhr);
+  fs.writeFileSync(`${shortHash(reportName)}.html`, reportHTML);
+  fs.writeFileSync(`${shortHash(reportName)}.json`, reportJSON);
+}
+
+/**
+ * @param  {String} pathToReportJSON - a path to a json file to load and parse
+ * @return {object|null} - the parsed report to work with
+ */
+function loadReport(pathToReportJSON) {
+  try {
+    const contents = fs.readFileSync(pathToReportJSON)
+    const report = JSON.parse(contents)
+    return report
+  } catch (error) {
+    console.error(`no parseable file found at ${pathToReportJSON}`);
+    return;
+  }
+}
+
+function loadTransferForPageVariants(pageName) {
+
+  const files = fs.readdirSync('./lh-runs/');
+  const matchingRuns = this.lighthouseRunsForName(files, pageName)
+  const rows = []
+
+  for (const runFile of matchingRuns) {
+
+    const loadPath = `./lh-runs/${runFile}`
+
+    if (fs.statSync(loadPath)) {
+      const result = this.loadReport(loadPath)
+      const transfer = this.analyseTransfer(result)
+      const variant = runFile.replace(`${pageName}-`, "").replace('.json', '')
+      transfer.Name = variant
+      rows.push(transfer)
+    }
+  }
+  return rows
+}
+
+
+/**
+ * @param  {LighthouseResultObject} lighthouseResult
+ *
+ * Accept a Lighthouse Result, and pull out the total transfer,
+ * broken down by file type.
+ *
+ */
+function analyseTransfer(lighthouseResult, skipWebArchive) {
+  const items = lighthouseResult.audits['network-requests'].details.items
+
+  function breakdownByType(transferByType, item) {
+
+
+    const fileTypes = [
+      "Document",
+      "Script",
+      "Stylesheet",
+      "Image",
+      "XHR",
+      "Font",
+    ]
+
+    // reject resource types we aren't interested in
+    if (!fileTypes.includes(item.resourceType)) {
+      return transferByType
+    }
+
+    // return early to skip counting web archive page furniture
+    // all urls from the original capture begin with the string below
+    const telltaleWebArchiveCapturePath = "web.archive.org/web"
+    if (!item.url.includes(telltaleWebArchiveCapturePath) && skipWebArchive) {
+      return transferByType
+    }
+
+    if (transferByType[item.resourceType]) {
+      transferByType[item.resourceType] += item.transferSize
+    } else {
+      transferByType[item.resourceType] = item.transferSize
+    }
+    return transferByType
+  }
+  return items.reduce(breakdownByType, {})
+}
+/**
+ * @param  {string} files - a set of file names from listing the
+ * contents of a directory
+ * @param  {string} fileName - file name to search for matching runs for
+ *
+ * Return the matching json files from the list that match the string fileName
+ */
+function lighthouseRunsForName(files, fileName) {
+  return files
+    .filter((file) => file.startsWith(fileName))
+    .filter((file) => file.match('.json'))
+}
