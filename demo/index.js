35baf4d00d082f9f88a962aff2ae9c5106d4bee6
Fershad <itsfish@fershad.com>
2022-10-16 16:49:47 +0800

    add a demo

---
+++ demo/index.js
@@ -0,0 +1,12 @@
+import { crawlSite } from '../index.js'
+
+/**
+ *
+ * @param {string} siteUrl
+ * @param {string} model
+ * @param {number} pageBudget
+ */
+
+crawlSite({
+    siteUrl: 'https://aremythirdpartiesgreen.com/',
+})
