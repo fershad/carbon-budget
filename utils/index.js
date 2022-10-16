722e6197568759de569a9045e25edd3fcd62a85f
Fershad <itsfish@fershad.com>
2022-10-16 11:20:28 +0800

    a more robust sanitiser

--- utils/index.js
+++ utils/index.js
@@ -1,4 +1,9 @@
 export const sanitiseUrlPath = (url) => {
     const { pathname } = new URL(url)
-    return pathname.replace(/\//g, '_')
+    // slugify the pathname, replacing all special characters with a dash
+    const slug = pathname.replace(/[^a-zA-Z0-9]/g, '-')
+    // remove any double dashes, leading and trailing dashes
+    const cleanSlug = slug.replace(/--/g, '-').replace(/^-|-$/g, '')
+
+    return cleanSlug.length > 0 ? cleanSlug : 'index'
 }
